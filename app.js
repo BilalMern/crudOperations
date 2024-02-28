0aa3084a656646b4a2aeafbbe40bcd7959894ce1
BilalMern <bilal.mern@gmail.com>
2024-02-29 00:48:19 +0500

    form validation

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 let form = document.getElementById("form");
+let input = document.getElementById("input")
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     console.log("button clicked")
+    formValidation()
+
 })
+let formValidation =()=>{
+    if(input.value === ""){
+        console.log("failure")
+    }
+    else{
+        console.log("success")
+    }
+}
