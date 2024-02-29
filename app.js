fa34299ca6e5842cfd58e821738c9b96fabe5a22
BilalMern <bilal.mern@gmail.com>
2024-02-29 12:36:06 +0500

    create post function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg =document.getElementById("msg");
+let posts=document.getElementById("posts")
+
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     
@@ -14,5 +16,23 @@ let formValidation =()=>{
     else{
         msg.innerHTML =""
         console.log("success")
+        acceptData()
     }
+}
+
+let data ={};
+let acceptData =()=>{
+    data["text"]=input.value;
+    createPosts()
+}
+
+let createPosts=()=>{
+    posts.innerHTML += `<div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i class="bi bi-pencil-square"></i>
+      <i class="bi bi-trash3"></i>
+    </span>
+  </div>`
+    input.value ="";
 }
