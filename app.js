4b2da15806fdda821c775cc7ce82212264e1e52f
BilalMern <bilal.mern@gmail.com>
2024-03-01 19:14:55 +0500

    making first submit

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-let form = document.getElementById("form")
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let textDate = document.getElementById("textDate");
+let textarea = document.getElementById("textarea")
 form.addEventListener("submit", (e)=>{
 e.preventDefault()
+formValidation()
 })
+
+let formValidation =()=>{
+    if(textInput.value === ""){
+        console.log("failure")
+    }else if(textDate.value ===""){
+        console.log("failure")
+    }
+    else{
+        console.log("success")
+    }
+}
